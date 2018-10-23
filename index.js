456e400cc4a83d3f45f32aa11bda28e11c9ee78f
dactue1006 <dactue1006@gmail.com>
2018-10-23 23:47:33 +0700

    use ejs in express

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = new express();
 
 const port = 8000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', (request, response)=>{
-  response.send('<h1>Hello NodeJS</h1>');
+  response.render('home', { title: 'hello express', link: '/user'});
+})
+
+app.get('/user', (req, res) => {
+  res.send('user list');
 })
 app.listen(port, () => {
   console.log('Server is running on port 8000');
