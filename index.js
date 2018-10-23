589db5590127a542fce97ef4ccd2cdbac400be25
dactue1006 <dactue1006@gmail.com>
2018-10-24 00:43:04 +0700

    query parameter

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = new express();
 
 const port = 8000;
-
+const users = [
+  { id: 1, name:'nguyen van a'},
+  { id: 2, name: 'nguyen b' }, 
+  { id: 3, name: 'nguyen c' }
+];
+  
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -11,8 +16,25 @@ app.get('/', (request, response)=>{
 })
 
 app.get('/user', (req, res) => {
-  res.send('user list');
+  res.render('user', { 
+    data: users,
+    preText: ''
+  });
 })
+
+app.get('/user/search', (req, res) => {
+  let q = req.query.q;
+  console.log(q);
+  let matchUsers = users.filter( (user)=>{
+    return user.name.indexOf(q) !== -1;
+  })
+  res.render('user', {
+    data: matchUsers,
+    preText: String(q)
+  })
+  //console.log(req.query);
+})
+
 app.listen(port, () => {
   console.log('Server is running on port 8000');
 })
