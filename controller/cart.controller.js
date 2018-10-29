2c5972d16508cc1c22dd034cfb08070f417f8bcd
dactue1006 <dactue1006@gmail.com>
2018-10-29 10:18:00 +0700

    add controller cart

---
+++ controller/cart.controller.js
@@ -0,0 +1,20 @@
+const db = require('../db');
+
+module.exports.addToCart = (req, res, next) => {
+  const productId = req.params.productId;
+  const sessionId = req.signedCookies.sessionId;
+
+  if (!sessionId) {
+    res.redirect('/products');
+    return ;
+  }
+  let count = db.get('session')
+                .find({ id: sessionId })
+                .get("cart." + productId)
+                .value() || 0;
+  db.get('session')
+    .find({ id: sessionId })
+    .set("cart." + productId, count+1)
+    .write();
+  res.redirect('/products');
+}
